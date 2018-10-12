17b45310fb92bcba3bb5881e1554c74874bc8951
Vladyslav Kokoshko <kokoshkovlad@gmail.com>
2018-10-12 14:36:32 +0200

    add router and route for register and login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import MainMenu from './components/MainMenu';
 import RegistrationContainer from './containers/RegistrationContainer';
+import LoginContainer from './containers/LoginContainer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <MainMenu />
-        </header>
-        <main>
-          <RegistrationContainer />
-        </main>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <MainMenu />
+          </header>
+          <main>
+            <Route exact path="/register" component={RegistrationContainer} />
+            <Route exact path="/login" component={LoginContainer} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
